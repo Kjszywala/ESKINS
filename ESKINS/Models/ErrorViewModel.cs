e87f7483f7248a52ee8c4c4257d73fdde22a3770
Kjszywala <kamil.szywala@magicard.com>
2023-03-15 11:06:24 +0000

    Repo created, Projects added

---
+++ ESKINS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESKINS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
