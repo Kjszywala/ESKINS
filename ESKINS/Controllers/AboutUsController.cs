861992c044d68ac0e864c211ff87e1b6e779204c
Kjszywala <kamil.szywala@magicard.com>
2023-03-21 11:38:11 +0000

    About us created, not sure if it matching

---
+++ ESKINS/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
