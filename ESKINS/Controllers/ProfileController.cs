1bcdda17d11e918c17325530cc72aa2de622d89b
kjszywala <kjszywala@protonmail.com>
2023-03-25 13:42:32 +0000

    Layout corrected

---
+++ ESKINS/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
