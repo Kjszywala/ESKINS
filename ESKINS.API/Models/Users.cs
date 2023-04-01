4aafd491bea5caac98f97843c6b8bc504dddc594
kjszywala <kjszywala@protonmail.com>
2023-04-01 12:46:59 +0100

    Users model added

---
+++ ESKINS.API/Models/Users.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESKINS.API.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Notes { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public bool IsActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string FirsName { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string Email { get; set; }
+
+        public decimal AccountBalance { get; set; }
+    }
+}
---
+++ ESKINS.API/Models/UsersAddresses.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ESKINS.API.Models
+{
+    public class UsersAddresses
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Notes { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public bool IsActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        public string? HouseNumber { get; set; }
+
+        public string? StreetName { get; set; }
+
+        public string? PostCode { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Country { get; set; }
+
+    }
+}
