b005bdaae77d4e65ee9b183bfec802c20c4557df
kjszywala <kjszywala@protonmail.com>
2023-04-01 13:05:59 +0100

    Exteriors model Added

---
+++ ESKINS.API/Models/Exteriors.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESKINS.API.Models
+{
+    public class Exteriors
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Notes { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public bool IsActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string Exterior { get; set; }
+    }
+}
