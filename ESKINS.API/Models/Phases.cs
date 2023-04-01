27ba564ccf936655ee7f5700737eda60ce9d8110
kjszywala <kjszywala@protonmail.com>
2023-04-01 13:02:35 +0100

    Phases table added

---
+++ ESKINS.API/Models/Phases.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ESKINS.API.Models
+{
+    public class Phases
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Notes { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public bool IsActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        public string Quality { get; set; }
+    }
+}
