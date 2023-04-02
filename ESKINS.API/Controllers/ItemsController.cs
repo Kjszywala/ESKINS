7aa3e30cb0ed1f5546563c7956f415387605b07a
kjszywala <kjszywala@protonmail.com>
2023-04-02 14:47:04 +0100

    Small corrections

--- ESKINS.API/Controllers/ItemsController.cs
+++ ESKINS.API/Controllers/ItemsController.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ESKINS.API.Controllers
 {
-    [[ApiVersion("1.0")]
+    [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class ItemsController : ControllerBase
