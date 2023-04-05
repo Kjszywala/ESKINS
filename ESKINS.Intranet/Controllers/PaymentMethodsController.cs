5bf94414a2b72311f22f6a534c43e16f6f116658
kjszywala <kjszywala@protonmail.com>
2023-04-05 20:53:20 +0100

    Got first data from db to the view

---
+++ ESKINS.Intranet/Controllers/PaymentMethodsController.cs
@@ -0,0 +1,20 @@
+﻿using ESKINS.DbServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Intranet.Controllers
+{
+    public class PaymentMethodsController : Controller
+    {
+        IPaymentMethodsServices paymentMethodsServices;
+
+        public PaymentMethodsController(IPaymentMethodsServices _paymentMethodsServices)
+        {
+            paymentMethodsServices = _paymentMethodsServices;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await paymentMethodsServices.GetAllAsync();
+            return View(model);
+        }
+    }
+}
