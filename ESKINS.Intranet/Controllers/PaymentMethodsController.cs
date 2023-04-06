9986f44a5ca84a3614339065f8314950549923e6
Kjszywala <kamil.szywala@magicard.com>
2023-04-06 08:28:42 +0100

    Regions and comments added

--- ESKINS.Intranet/Controllers/PaymentMethodsController.cs
+++ ESKINS.Intranet/Controllers/PaymentMethodsController.cs
@@ -5,16 +5,30 @@ namespace ESKINS.Intranet.Controllers
 {
     public class PaymentMethodsController : Controller
     {
+        #region Variables
+
         IPaymentMethodsServices paymentMethodsServices;
+
+        #endregion
+
+        #region Constructor
 
         public PaymentMethodsController(IPaymentMethodsServices _paymentMethodsServices)
         {
             paymentMethodsServices = _paymentMethodsServices;
         }
+
+        #endregion
+
+        #region Controllers
+
         public async Task<IActionResult> IndexAsync()
         {
             var model = await paymentMethodsServices.GetAllAsync();
             return View(model);
         }
+
+        #endregion
+
     }
 }
