230f1b7a14b9cbe4f707655e9abe3aff9aa6e16f
kjszywala <kjszywala@protonmail.com>
2023-04-13 19:41:58 +0100

    Items done

--- ESKINS.Intranet/Controllers/ItemsController.cs
+++ ESKINS.Intranet/Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using ESKINS.BusinessLogic.Interfaces;
 using ESKINS.DbServices.Interfaces;
 using ESKINS.DbServices.Models;
+using ESKINS.DbServices.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -85,62 +86,44 @@ namespace ESKINS.Intranet.Controllers
             }
         }
 
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            try
+            {
+                var model = itemsServices.GetAsync(id);
+
+                ViewBag.ItemLogsList = new SelectList(await itemLogsServices.GetAllAsync(), "Id", "Title");
+                ViewBag.ItemCollectionList = new SelectList(await itemCollectionsServices.GetAllAsync(), "Id", "ItemCollection");
+                ViewBag.UserList = new SelectList(await usersServices.GetAllAsync(), "Id", "Email");
+                ViewBag.ItemLocationList = new SelectList(await itemLocationsServices.GetAllAsync(), "Id", "ItemLocation");
+                ViewBag.CategoryList = new SelectList(await categoriesServices.GetAllAsync(), "Id", "CategoryDescription");
+                ViewBag.PhaseList = new SelectList(await phasesServices.GetAllAsync(), "Id", "Phase");
+                ViewBag.QualityList = new SelectList(await qualitiesServices.GetAllAsync(), "Id", "Quality");
+                ViewBag.ExteriorList = new SelectList(await exteriorsServices.GetAllAsync(), "Id", "Exterior");
+                if (model == null)
+                {
+                    return View("Error");
+                }
+                return View(model.Result);
+            }
+            catch (Exception e)
+            {
+                await errorLogsServices.Error(e);
+                return View("Error");
+            }
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetItemAsync(int id)
         {
-            var exteriors = await itemsServices.GetAsync(id);
+            var item = await itemsServices.GetAsync(id);
 
-            if (exteriors == null)
+            if (item == null)
             {
                 return NotFound();
             }
 
-            var exteriorsModel = new ItemsModels
-            {
-                Id = exteriors.Id,
-                Title = exteriors.Title,
-                IsActive = exteriors.IsActive,
-                CreationDate = exteriors.CreationDate,
-                ModificationDate = exteriors.ModificationDate,
-                SerialNumber = exteriors.SerialNumber,
-                ActualPrice = exteriors.ActualPrice,
-                Category = exteriors.Category,
-                CategoryId = exteriors.CategoryId,
-                Discount = exteriors.Discount,
-                Exterior = exteriors.Exterior,
-                ExteriorId = exteriors.ExteriorId,
-                ItemCollection = exteriors.ItemCollection,
-                ItemCollectionId = exteriors.ItemCollectionId,
-                ItemFloat = exteriors.ItemFloat,
-                ItemImage = exteriors.ItemImage,
-                ItemLocation = exteriors.ItemLocation,
-                ItemLocationId = exteriors.ItemLocationId,
-                ItemLog = exteriors.ItemLog,
-                ItemLogsId = exteriors.ItemLogsId,
-                ItemPriceHistories = exteriors.ItemPriceHistories,
-                Notes = exteriors.Notes,
-                OnSale = exteriors.OnSale,
-                Pattern = exteriors.Pattern,
-                Phase = exteriors.Phase,
-                PhaseId = exteriors.PhaseId,
-                ProductName = exteriors.ProductName,
-                Quality = exteriors.Quality,
-                QualityId = exteriors.QualityId,
-                Souvenir = exteriors.Souvenir,
-                StatTrack = exteriors.StatTrack,
-                User = exteriors.User,
-                UserId = exteriors.UserId
-            };
-            //ViewBag.ItemLogsList = new SelectList(await itemLogsServices.GetAllAsync(), "Id", "Title");
-            //ViewBag.ItemCollectionList = new SelectList(await itemCollectionsServices.GetAllAsync(), "Id", "ItemCollection");
-            //ViewBag.UserList = new SelectList(await usersServices.GetAllAsync(), "Id", "Email");
-            //ViewBag.ItemLocationList = new SelectList(await itemLocationsServices.GetAllAsync(), "Id", "ItemLocation");
-            //ViewBag.CategoryList = new SelectList(await categoriesServices.GetAllAsync(), "Id", "CategoryDescription");
-            //ViewBag.PhaseList = new SelectList(await phasesServices.GetAllAsync(), "Id", "Phase");
-            //ViewBag.QualityList = new SelectList(await qualitiesServices.GetAllAsync(), "Id", "Quality");
-            //ViewBag.ExteriorList = new SelectList(await exteriorsServices.GetAllAsync(), "Id", "Exterior");
-
-            return Ok(exteriors);
+            return Ok(item);
         }
 
         public async Task<IActionResult> CreateAsync()
@@ -217,14 +200,44 @@ namespace ESKINS.Intranet.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> EditAsync(int id, ItemsModels model)
+        public async Task<IActionResult> EditItemAsync(int ids, ItemsModels model)
         {
             try
             {
-                var oldModel = await itemsServices.GetAsync(id);
+                var oldModel = await itemsServices.GetAsync(ids);
+                var file = Request.Form.Files.FirstOrDefault();
+                if (file != null && file.Length > 0)
+                {
+                    using (var stream = file.OpenReadStream())
+                    {
+                        using (var binaryReader = new BinaryReader(stream))
+                        {
+                            var imageData = binaryReader.ReadBytes((int)file.Length);
+                            model.ItemImage = imageData;
+                        }
+                    }
+                }
+                else
+                {
+                    model.ItemImage = oldModel.ItemImage;
+                }
                 model.ModificationDate = DateTime.Now;
                 model.CreationDate = oldModel.CreationDate;
-                var IsConfirmed = await itemsServices.EditAsync(id, model);
+                foreach (var modelStateEntry in ModelState)
+                {
+                    var propertyName = modelStateEntry.Key;
+                    var propertyValue = modelStateEntry.Value;
+
+                    if (propertyValue.ValidationState == ModelValidationState.Valid)
+                    {
+                        // Property is valid, continue with your logic here
+                    }
+                    else
+                    {
+                        // Property is invalid, handle the error here
+                    }
+                }
+                var IsConfirmed = await itemsServices.EditAsync(ids, model);
                 if (IsConfirmed)
                 {
                     return RedirectToAction("Index");
