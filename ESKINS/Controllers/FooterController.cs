15545b08dc6ab3c59699feb0a292ab0505d5eab6
kjszywala <kjszywala@protonmail.com>
2023-05-13 11:46:55 +0100

    Footer links done with views

---
+++ ESKINS/Controllers/FooterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Controllers
+{
+    public class FooterController : Controller
+    {
+        public IActionResult JobOpening()
+        {
+            return View();
+        }
+        public IActionResult EmployeeSuccess()
+        {
+            return View();
+        }
+        public IActionResult Benefits()
+        {
+            return View();
+        }
+		public IActionResult Legacy()
+		{
+			return View();
+		}
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Hosting()
+        {
+            return View();
+        }
+        public IActionResult Development()
+        {
+            return View();
+        }
+        public IActionResult WebDesign()
+        {
+            return View();
+        } 
+        public IActionResult Company()
+        {
+            return View();
+        }
+    }
+}
