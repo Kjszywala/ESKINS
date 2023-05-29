269ebea0c45ce0d537dcc18325c56817ea682229
kjszywala <kjszywala@protonmail.com>
2023-05-29 19:44:08 +0100

    Item Details done

---
+++ ESKINS/Controllers/BuyCartComponent.cs
@@ -0,0 +1,42 @@
+﻿using ESKINS.DbServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Controllers
+{
+    public class BuyCartComponent : ViewComponent
+    {
+        #region Variables
+
+        ICartServices cartServices;
+        IErrorLogsServices errorLogsServices;
+
+        #endregion
+
+        #region Constructor
+
+        public BuyCartComponent(ICartServices _cartServices, IErrorLogsServices _errorLogsServices)
+        {
+            cartServices = _cartServices;
+            errorLogsServices = _errorLogsServices;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                var list = cartServices.GetAllAsync().Result.Where(item => item.SessionId == Config.SessionId).ToList();
+                return View("BuyCartComponent", list);
+            }
+            catch (Exception ex)
+            {
+                await errorLogsServices.Error(ex);
+                return View("Error");
+            }
+        }
+        #endregion
+    }
+}
