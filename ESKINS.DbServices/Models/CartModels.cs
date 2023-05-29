0c9793687fb47a5bbaf209c78de0d7979c5594a5
kjszywala <kjszywala@protonmail.com>
2023-05-29 12:34:34 +0100

    Cart model added to dbservices

---
+++ ESKINS.DbServices/Models/CartModels.cs
@@ -0,0 +1,12 @@
+﻿namespace ESKINS.DbServices.Models
+{
+    public class CartModels
+    {
+        public int Id { get; set; }
+        public string SessionId { get; set; }
+        public int? ItemId { get; set; }
+        public ItemsModels? Item { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
