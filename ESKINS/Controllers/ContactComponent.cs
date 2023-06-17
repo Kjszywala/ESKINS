6a314fe1a83eb85daf760332762d77521f6232f0
kjszywala <kjszywala@protonmail.com>
2023-06-17 14:16:07 +0100

    Contact controller changed to component

---
+++ ESKINS/Controllers/ContactComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESKINS.Controllers
+{
+    public class ContactComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("ContactComponent");
+        }
+    }
+}
